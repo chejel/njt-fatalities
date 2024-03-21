93a4d5fd903bb141e3ffe043298e77b863460d65
chejel <chejenj@gmail.com>
2024-03-21 19:10:29 -0400

    Deploy to GHP

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
     kit: {
         adapter: adapter(),
         paths: {
-            base: process.env.NODE_ENV === 'production' ? '/njt-fatalities' : '',
+			base: process.argv.includes('dev') ? '' : '/njt-fatalities',
         }
     }
 };
